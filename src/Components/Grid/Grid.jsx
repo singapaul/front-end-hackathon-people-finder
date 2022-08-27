31791402f11fb5e60f8d5ef8744b1fe85f94187b
PAUL HARDMAN <89204135+singapaul@users.noreply.github.com>
2022-08-27 15:46:37 +0100

    all

--- src/Components/Grid/Grid.jsx
+++ src/Components/Grid/Grid.jsx
@@ -1,11 +1,11 @@
 import ProfileCard from "../ProfileCard/ProfileCard";
 import "./Grid.scss";
 
-const Grid = ({ profiles }) => {
+const Grid = ({ cards }) => {
   return (
     <div className="Grid">
-      {profiles.map((profile) => {
-        return <ProfileCard profile={profile} key={profile.id} />;
+      {cards.map((card) => {
+        return <ProfileCard profile={card} key={card.id} />;
       })}
     </div>
   );
